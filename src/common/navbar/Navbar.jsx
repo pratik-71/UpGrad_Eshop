f9a451d85e55de5ef8d4ccf093205205c7afa386
pratik-71 <pratikdabhade66344@gmail.com>
2024-02-26 11:37:18 +0530

    Navbar done

--- src/common/navbar/Navbar.jsx
+++ src/common/navbar/Navbar.jsx
@@ -8,7 +8,6 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -24,52 +23,54 @@ const loggedIn = [
   },
 ];
 
-const Navbar = (props) => {
-  const [navlinks, setnavlinks] = React.useState(pages);
-  const [loggedIn, setloggedIn] = React.useState(true);
-  const [isadmin,setisadmin] = React.useState(true)
 
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto",
-    },
-  }));
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
 
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    width: "100%", 
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      [theme.breakpoints.up("sm")]: {
-        width: "25ch", 
-        "&:focus": {
-          width: "30ch", 
-        },
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '25ch',
+      '&:focus': {
+        width: '30ch',
       },
     },
-  }));
+  },
+}));
 
+
+
+const Navbar = (props) => {
+  const [navlinks, setNavLinks] = React.useState(pages);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(true);
+  const [isAdmin, setIsAdmin] = React.useState(false);
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -81,15 +82,12 @@ const Navbar = (props) => {
     setAnchorElNav(null);
   };
 
-
   return (
     <>
       <AppBar position="static">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <ShoppingCartIcon
-              sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
-            />
+            <ShoppingCartIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
             <Typography
               variant="h6"
               noWrap
@@ -106,15 +104,25 @@ const Navbar = (props) => {
               upGrad Eshop
             </Typography>
 
+
+
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex", justifyContent: "flex-start" },
+              }}
+            >
             <Search>
               <SearchIconWrapper>
                 <SearchIcon />
               </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ "aria-label": "search" }}
-              />
+              <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
             </Search>
+            </Box>
+
+
+
+
 
             <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
               <IconButton
@@ -127,6 +135,11 @@ const Navbar = (props) => {
               >
                 <MenuIcon />
               </IconButton>
+
+
+
+
+
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorElNav}
@@ -145,22 +158,42 @@ const Navbar = (props) => {
                   display: { xs: "block", md: "none" },
                 }}
               >
-
+                <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase placeholder="Search…" inputProps={{ "aria-label": "search" }} />
+            </Search>
+                
+                 {isLoggedIn && (
+                  <>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">Home</Typography>
+                    </MenuItem>
+                    {isAdmin && (
+                      <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">Add Products</Typography>
+                      </MenuItem>
+                    )}
+                  </>
+                )}
 
                 {pages.map((page) => (
                   <MenuItem key={page} onClick={handleCloseNavMenu}>
                     <Typography textAlign="center">{page}</Typography>
                   </MenuItem>
                 ))}
+
               </Menu>
+
+
+
+
+
+
             </Box>
 
-
-
-
-            <ShoppingCartIcon
-              sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
-            />
+            <ShoppingCartIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
             <Typography
               variant="h5"
               noWrap
@@ -170,7 +203,6 @@ const Navbar = (props) => {
                 mr: 2,
                 display: { xs: "flex", md: "none" },
                 flexGrow: 1,
-
                 fontWeight: 700,
                 color: "inherit",
                 textDecoration: "none",
@@ -179,17 +211,14 @@ const Navbar = (props) => {
               upGrad Eshop
             </Typography>
 
-
-
             <Box
               sx={{
                 flexGrow: 1,
                 display: { xs: "none", md: "flex", justifyContent: "flex-end" },
               }}
             >
-              {loggedIn ? (
+              {isLoggedIn ? (
                 <>
-
                   <Button
                     onClick={handleCloseNavMenu}
                     sx={{
@@ -203,26 +232,21 @@ const Navbar = (props) => {
                     Home
                   </Button>
 
-
-                     
-                  {
-                        isadmin?
-                        <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{
-                          my: 2,
-                          mx: 1,
-                          color: "white",
-                          display: "block",
-                          border: "1px solid white",
-                        }}
-                        variant="outlined"
-                      >
-                        Add Products
-                      </Button>
-                      :
-                      <div></div>
-                     }
+                  {isAdmin && (
+                    <Button
+                      onClick={handleCloseNavMenu}
+                      sx={{
+                        my: 2,
+                        mx: 1,
+                        color: "white",
+                        display: "block",
+                        border: "1px solid white",
+                      }}
+                      variant="outlined"
+                    >
+                      Add Products
+                    </Button>
+                  )}
 
                   <Button
                     onClick={handleCloseNavMenu}
@@ -237,10 +261,7 @@ const Navbar = (props) => {
                   >
                     Log Out
                   </Button>
-
                 </>
-
-
               ) : (
                 navlinks.map((page) => (
                   <Button
@@ -260,9 +281,6 @@ const Navbar = (props) => {
                 ))
               )}
             </Box>
-
-
-
           </Toolbar>
         </Container>
       </AppBar>
